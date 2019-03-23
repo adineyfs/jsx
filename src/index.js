c6f8fe9da096d9f4a0ed77e5e1c4c10ddc0016c2
Adiney Ferreira <adsilva.5a@bionexo.com>
2019-03-23 14:48:24 -0300

    Fim da primeira seção

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>;
+  const buttonText = {text: 'Click Me'};
+  const style = { backgroundColor: 'blue', color: 'white' };
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={style}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 }
 
 // Take the react component and show it on the screen
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 );
